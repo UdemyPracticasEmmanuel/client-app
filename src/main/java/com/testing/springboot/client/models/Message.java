e36fb014648913cbd56c2fb0f3c1825e2e8f23e0
UdemyPracticasEmmanuel <emmanuel.lopez@neoris.com>
2024-06-19 15:32:30 -0700

    Se agregan capas de controllers y models

---
+++ src/main/java/com/testing/springboot/client/models/Message.java
@@ -0,0 +1,20 @@
+package com.testing.springboot.client.models;
+
+public class Message {
+    private String text;
+
+    public Message(String text) {
+        this.text = text;
+    }
+
+    public Message() {
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
